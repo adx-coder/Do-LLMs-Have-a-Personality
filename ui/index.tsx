d8ac1bd5e20131fca1b2d892a290aeb192fa9073
Ashwath David <74618918+adx-coder@users.noreply.github.com>
2025-12-16 20:50:25 -0500

    Delete ui directory

--- ui/index.tsx
+++
@@ -1,5 +0,0 @@
-/* 
-  This file is no longer used. 
-  The application has been converted to a standalone HTML/CSS/JS file (index.html) 
-  for easy GitHub Pages deployment without a build step.
-*/
